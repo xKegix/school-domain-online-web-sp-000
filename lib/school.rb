f73ab9dc8e43f09a50b85b22315c57df809fe872
Keginold Philip <keginoldp@hotmail.com>
2021-02-04 17:07:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+	class School
+		attr_accessor :roster
+		attr_reader :name
+
+		def initialize(name)
+			@name = name
+			@roster = roster
+		end
+
+	end
+
+	school = School.new("Bayside High School")
