0dda27d30ef352c8107e55def41738a39f45a90c
Keginold Philip <keginoldp@hotmail.com>
2021-02-04 17:25:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,11 @@
 		end
 
 		def add_student(name, grade)
-			@roster[grade] = []
-			@roster[grade].push(name)
+			if @roster.include?(grade)
+				@roster[grade] << name
+			else 
+				@roster[grade] = []
+				@roster[grade] << name
 		end
 
 	end
